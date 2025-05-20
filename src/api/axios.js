5ae5f2cb168ce954c7ebc8c9d658909444f088df
juanchoalcoba <juanchoalcoba@gmail.com>
2025-05-20 14:33:21 -0300

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const instance = axios.create({
-    baseURL: `http://backend-rey.vercel.app/api`,
+    baseURL: `https://backend-rey.vercel.app/api`,
     withCredentials: true
 })
 export default instance
