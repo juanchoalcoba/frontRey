5fa9855cc8512b6cbae2362d755eee460a5e7435
juanchoalcoba <juanchoalcoba@gmail.com>
2025-05-22 21:27:24 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ function App() {
             <Route path="/login" element={<LoginPage />} />
 
             {/* Rutas protegidas */}
-            <Route element={<ProtectedRoute />}>
               <Route path="/tienda" element={<Dashboard />} />
+            <Route element={<ProtectedRoute />}>
               <Route path="/contacto" element={<Contacto />} />
               <Route path="/productos" element={<Productos />} />
               <Route path="/profile" element={<ProfilePage />} />
